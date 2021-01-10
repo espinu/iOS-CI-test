8a4f3d166e2d395567b8d0946ee091cca00ea132
Eugen Spinu <eugen.spinu@gmail.com>
2021-01-10 23:09:35 +0200

    First commit

---
+++ CI-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CI-test
+//
+//  Created by Eugen Spinu on 10/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
